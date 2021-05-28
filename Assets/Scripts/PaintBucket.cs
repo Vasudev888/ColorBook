e47cfc3e1de96137f6e80932c006a98a3fba9938
Vasudev Kulkarni <vdk5384@gmail.com>
2021-05-28 23:41:54 +0530

    inital commit

---
+++ Assets/Scripts/PaintBucket.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PaintBucket : MonoBehaviour
+{
+
+    public Color[] colorList;
+    public Color curColor;
+    public int colorCount;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        curColor = colorList[colorCount];
+        var ray = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray, -Vector2.up);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(hit.collider != null)
+            {
+                SpriteRenderer sp = hit.collider.gameObject.GetComponent<SpriteRenderer>();
+                Debug.Log(hit.collider.name);
+
+                sp.color = curColor;
+            }
+
+            if (hit.collider == null)
+            {
+                Camera.main.backgroundColor = curColor;
+            }
+        }
+
+
+        
+    }
+
+    public void Paint(int colorCode)
+    {
+        colorCount = colorCode;
+    }
+}
+ 
